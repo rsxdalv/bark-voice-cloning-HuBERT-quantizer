c57f33e764fbf9e047a6d6943ea4f555dd9a3be7
Roberts Slisans <roberts.slisans@gmail.com>
2023-07-03 21:40:56 +0300

    change package name

--- setup.py
+++ setup.py
@@ -1,11 +1,8 @@
 from setuptools import setup
 
 setup(
-    name='bark_voice_clone',
+    name='bark_hubert_quantizer',
     version='0.0.1',
-    # description='Your package description',
-    # author='Your Name',
-    # author_email='your@email.com',
     packages=['hubert'],
     install_requires=[
         'audiolm-pytorch==1.1.4',
