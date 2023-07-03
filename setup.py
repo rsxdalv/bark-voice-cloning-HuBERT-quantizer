edabfbc3661afd0dbf3804165161fcd217a1f3ba
Roberts Slisans <roberts.slisans@gmail.com>
2023-07-03 20:49:06 +0300

    add requires

--- setup.py
+++ setup.py
@@ -1,5 +1,20 @@
 from setuptools import setup
 
 setup(
+    name='bark_voice_clone',
+    version='0.0.1',
+    # description='Your package description',
+    # author='Your Name',
+    # author_email='your@email.com',
     packages=['hubert'],
+    install_requires=[
+        # 'audiolm-pytorch',
+        # 'fairseq',
+        # 'huggingface-hub',
+        # 'sentencepiece',
+        # 'transformers',
+        'encodec',
+        # 'soundfile; platform_system == "Windows"',
+        # 'sox; platform_system != "Windows"'
+    ],
 )
