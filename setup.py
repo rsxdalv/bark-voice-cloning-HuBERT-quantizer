ce93b293222586514755a9a4fbce00b2408ec9eb
Roberts Slisans <rsxdalv@gmail.com>
2024-11-19 01:10:49 +0200

    improve compatibility (#2)
    * widen audiolm-pytorch version to 1.2.29
    
    * bump version
    
    * add build wheel
    
    * fix pip

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='bark_hubert_quantizer',
-    version='0.0.4',
+    version='0.0.5',
     packages=['bark_hubert_quantizer'],
     install_requires=[
-        'audiolm-pytorch==1.1.4',
+        'audiolm-pytorch>=1.1.4,<1.2.29',
         'fairseq',
         'huggingface-hub',
         'sentencepiece',
